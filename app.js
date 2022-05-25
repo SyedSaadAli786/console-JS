d1824566dfa5e81837bd914ea66094534d9c007d
Syed Saad Ali <saadali23489@gmail.com>
2022-05-25 16:47:10 +0500

    console types

---
+++ app.js
@@ -0,0 +1,10 @@
+console.time("your code")
+console.log("hello world")  //string value
+console.log(32+4)          // integer value    
+console.log(true)          //boolean
+console.log(32,4,3,2)
+console.log({sub: "english"  , mark:88})
+console.table({saad: "this"  , mark:88})
+console.warn("this is warning")
+console.timeEnd("your code")
+//console.clear()
